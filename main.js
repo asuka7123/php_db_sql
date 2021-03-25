ee444414d75da6b75938031ce30995ce2d2f337a
asuka7123 <80142146+asuka7123@users.noreply.github.com>
2021-03-25 23:55:10 +0900

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $(".yn_btn").on("click", function() {
-   $(this).closest("div").css("display","none");
-   id = $(this).attr("href");              
-   $(id).addClass("positionFit").show("fast");
-});
-});  
